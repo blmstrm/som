88e50083cea9e569c38d4e33fe4ef2aa09097d18
kalle <kalle.blomstrom@gmail.com>
2012-07-11 23:55:56 +0200

    Divided into methods for changing months and weekdays. Removed \n from lines in file.

--- readfile.rb
+++ readfile.rb
@@ -1,67 +1,80 @@
 #!/home/kalle/.rvm/rubies/ruby-1.9.3-p125/bin/ruby -w
-
-def readFile(inFile)
-  file = File.new(inFile,"r")
-  returnMatrix = Hash.new
-  lines = 0
-  y = 0
-  while  (line = file.gets)
-    if lines > 0
-      returnMatrix[y] = line.split(',')
-      y+=1
+#Read file to matrix, strip end of line
+  def file2Mtx(inFile,separator)
+    file = File.new(inFile,'r')
+    returnMatrix = Hash.new
+    lines = 0
+    y = 0
+    while  (line = file.gets)
+      if lines > 0
+        returnMatrix[y] = line.strip.split(separator)
+        y+=1
+      end
+      lines+=1
     end
-    lines+=1
+    file.close
+    return returnMatrix
   end
-  file.close
-  return returnMatrix
-end
 
-def adjustMatrix(matrix)
-  matrix.each{|key,array|
-    case array[2]
-    when "jan"
-      array[2] = 1
-    when "feb"
-       array[2] = 2
-    when "mar"
-       array[2] = 3
-    when "apr"
-       array[2] = 4
-    when "may"
-       array[2] = 5
-    when "jun"
-       array[2] = 6 
-    when "jul"
-       array[2] = 7
-    when "aug"
-      array[2]  = 8
-    when "sep"
-       array[2] = 9
-    when "oct"
-       array[2] = 10
-    when "nov"
-       array[2] = 11
-    when "dec"
-       array[2] = 12
-    end
+#Adjust "mon" to 1 etc
+  def adjustWeekDay(matrix,column)
+    matrix.each{|key,array|
+      case array[column]
+      when "mon"
+        array[column] = 1
+      when "tue"
+        array[column] = 2
+      when "wed"
+        array[column]= 3
+      when "thu"
+        array[column]= 4
+      when "fri"
+        array[column] = 5
+      when "sat"
+        array[column] = 6
+      when "sun"
+        array[column] = 7
+      end
+    }
+    return matrix
+  end
 
-    case array[3]
-    when "mon"
-       array[3] = 1
-    when "tue"
-       array[3] = 2
-    when "wed"
-       array[3]= 3
-    when "thu"
-       array[3]= 4
-    when "fri"
-      array[3] = 5
-    when "sat"
-      array[3] = 6
-    when "sun"
-      array[3] = 7
-    end
-  }
-  return matrix
-end
-puts adjustMatrix(readFile("forestfires.csv"))[0][3]
+#Adjust "jan" to 1 etc
+  def adjustMonth(matrix,column)
+    
+    matrix.each{|key,array|
+      case array[column]
+      when "jan"
+        array[column] = 1
+      when "feb"
+        array[column] = 2
+      when "mar"
+        array[column] = 3
+      when "apr"
+        array[column] = 4
+      when "may"
+        array[column] = 5
+      when "jun"
+        array[column] = 6 
+      when "jul"
+        array[column] = 7
+      when "aug"
+        array[column]  = 8
+      when "sep"
+        array[column] = 9
+      when "oct"
+        array[column] = 10
+      when "nov"
+        array[column] = 11
+      when "dec"
+        puts "dec"
+        array[column] = 12
+      end
+    }
+    return matrix
+  end
+
+mtx = file2Mtx("forestfires.csv",',')
+mtx = adjustMonth(mtx,2)
+mtx = adjustWeekDay(mtx,3)
+puts mtx
